b82fd27aa6ad7e9b5ea8a28abd55c6965ea64a04
Jaehyuk-Lee <alex00728@outlook.com>
2024-11-18 20:26:11 +0900

    use express

--- index.ts
+++ index.ts
@@ -1,4 +1,25 @@
+import express from 'express';
+const app = express();
 import * as query from './src/queries';
 
-query.insertUser({ name: 'Jaehyuk', email: 'a@a.com', age: 28 });
-query.getUsers().then(console.log);
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST');
+    res.header('Access-Control-Allow-Headers',
+        'Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization');
+    next();
+});
+
+app.get('/insert', async (req, res) => {
+    await query.insertUser({ name: 'Jaehyuk', email: 'a@a.com', age: 28 });
+    res.send('Insert Success');
+});
+
+app.get('/get', async (req, res) => {
+    const users = await query.getUsers();
+    res.send(users);
+});
+
+app.listen(4000, () => {
+    console.log('Server is running on http://localhost:4000');
+});
